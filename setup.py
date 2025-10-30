9c978fa225b6ba46fc99594773d2eebcdd81f378
Lukas Fichtner <lukasfichtner@gmx.de>
2025-10-30 10:21:21 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Football_Analysis_Tool',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='lukasfichtner',
+    author_email='',
+    description=''
+)
